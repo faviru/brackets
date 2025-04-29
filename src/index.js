6ae280024f3518c5c8bdd6b2af922291740307cc
Katherine <katerina0708@gmail.com>
2025-04-29 13:00:29 +0300

    fix: let to const, better conditions

--- src/index.js
+++ src/index.js
@@ -3,35 +3,29 @@ module.exports = function check(str, bracketsConfig) {
   const openBrackets = [],
     bracketsPairs = {};
 
-  bracketsConfig.map(el => {
+  bracketsConfig.forEach(el => {
     openBrackets.push(el[0]);
     bracketsPairs[el[1]] = el[0];
   });
 
-  let stack = [];
-  let arr = str.split('');
+  const stack = [];
+  const arr = str.split('');
 
   for (let i = 0; i < arr.length; i++) {
     const el = arr[i];
 
     if (openBrackets.includes(el)) {
-      if (bracketsPairs[el] !== el) {
+      if (bracketsPairs[el] !== el ||
+        (bracketsPairs[el] === el && (stack.length === 0 || stack[stack.length - 1] !== el))) {
         stack.push(el);
       } else {
-        if (stack.length === 0) {
-          stack.push(el);
-        } else if (stack[stack.length - 1] !== el) {
-          stack.push(el);
-        } else {
-          stack.pop()
-        }
+        stack.pop()
       }
     } else {
       if (stack.length === 0) {
         return false;
       }
-      const lastElement = stack[stack.length - 1];
-      if (lastElement === bracketsPairs[el]) {
+      if (stack[stack.length - 1] === bracketsPairs[el]) {
         stack.pop()
       } else {
         return false
